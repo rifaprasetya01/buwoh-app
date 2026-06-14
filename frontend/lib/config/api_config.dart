4bfa90eac84de4fe2594386b120b79ce02aaa3d3
Rifa Prasetya <rifaekaprasetya13@gmail.com>
2026-06-14 14:49:40 +0700

    chore: setup adb reverse proxy for local development and update event uploads

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.1.5:5000/api/v1'; // Menggunakan ADB reverse
+  static const String baseUrl = 'http://127.0.0.1:5000/api/v1'; // Menggunakan ADB reverse
   // static const String baseUrl = 'http://192.168.18.31:5000/api/v1'; 
-  // Gunakan IP ini jika test di HP fisik
+  // Gunakan IP ini jika test di HP fisik tanpa kabel USB
 
 
   
